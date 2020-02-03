011533d081e4514cf616f12fa9581b79629cd979
usmankhan-bit <usmankhannn18@gmail.com>
2020-02-03 22:30:28 +0530

    Bitwise operation

---
+++ uppertolower.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cin>>s;
+    for(int i=0;i<s.length();i++)
+    {
+        s[i]=s[i]|32;
+    }
+
+    cout<<s<<endl;
+}
